4626b80597f3ae9601bfe98aaf16e816e6aef457
Usama Abid <67735036+OsamaDS@users.noreply.github.com>
2021-06-30 03:01:32 +0500

    updated commit

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from flask import Flask, render_template, request
 from flask_cors import cross_origin
 import pickle
 
-app = Flask(__name__) # initializing a flask app
+application = Flask(__name__) # initializing a flask app
 #app=application
-@app.route('/', methods=['GET'])  # route to display the home page
+@application.route('/', methods=['GET'])  # route to display the home page
 @cross_origin()
 def homePage():
     return render_template("index.html")
 
-@app.route('/predict', methods=['POST', 'GET']) # route to show the predictions in a web UI
+@application.route('/predict', methods=['POST', 'GET']) # route to show the predictions in a web UI
 @cross_origin()
 def index():
     if request.method == 'POST':
@@ -46,4 +46,4 @@ def index():
 
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
-	app.run(debug=True) # running the app
+	application.run(debug=True) # running the app
